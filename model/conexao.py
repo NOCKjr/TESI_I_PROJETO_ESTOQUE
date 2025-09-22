ce52b7a8ad8498c0c76278232b99213823eff5b4
nock_jr <eliascruznocjr123@gmail.com>
2025-09-22 18:26:35 -0500

    adicionando banco ao model para não dar problemas de caminho

---
+++ model/conexao.py
@@ -0,0 +1,11 @@
+import sqlite3
+from sqlite3 import Error
+
+class Conexao:
+    def get_conexao(self):
+        caminho = 'banco.db'
+        try:
+            con = sqlite3.connect(caminho)
+            return con
+        except Error as er:
+            print(er)
