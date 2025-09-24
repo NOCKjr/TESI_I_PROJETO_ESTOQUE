89ae28b959779cb24c51e5485a6e019c53a0f196
emanuel-souza-ramos <emanuel.ramos@sou.ufac.br>
2025-09-24 12:11:58 -0500

    feat: tentativa de implementação do método para listar usuários

--- view/telas/tela_consultas.py
+++ view/telas/tela_consultas.py
@@ -1,12 +1,17 @@
 import tkinter as tk
+import ttkbootstrap as ttk
 import constants
 
 from view.telas.gerenciador_de_janelas import GerenciadorDeJanelasBase
 from view.telas.tela_interface import TelaInterface
+from control.usuario_controller import UsuarioController
+
 
 class TelaConsultas(TelaInterface):
     def __init__(self, master, gerenciador_de_janelas: GerenciadorDeJanelasBase, largura=constants.LARGURA_JANELA, altura=constants.ALTURA_JANELA):
         super().__init__(master, width=largura, height=altura, bg='#ffffff')
+
+        self.controle_usuarios = UsuarioController()
 
         # guarda qual objeto está gerenciando a troca entre janelas
         self.gerenciador_de_janelas = gerenciador_de_janelas
@@ -15,12 +20,17 @@ class TelaConsultas(TelaInterface):
         self.lbl_sigeme = tk.Label(self, text='SIGEME', bg=self.cores['branco'])
         self.lbl_sigeme.pack(side='top')
 
+        # Botão que ativa a consulta na lista de usuários
         self.btn_usuarios = tk.Button(self, text='Usuários', command=self.listar_usuarios)
         self.btn_usuarios.pack()
 
-
     def listar_usuarios(self):
-        pass
+        colunas = ['ID', 'LOGIN', 'SENHA', 'TIPO']
+        self.tvw_usuarios = ttk.Treeview(self, height=5, columns=colunas, show='headings')
+        tuplas = self.controle_usuarios.listar_usuario()
+        for item in tuplas:
+            self.tvw_usuarios.insert('', 'end', values=item)
+
 
     def mostrar(self):
         self.pack(expand=True, fill='both', anchor='center')
