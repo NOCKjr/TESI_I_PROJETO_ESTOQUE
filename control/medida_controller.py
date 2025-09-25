4d0267e2c6f9c006f41a3ae2cc7c633c585acd2f
elias.cruz <eliascruznocjr123@gmail.com>
2025-09-25 11:32:43 -0500

    feat: medida crud até o controller

---
+++ control/medida_controller.py
@@ -0,0 +1,21 @@
+from model import medida_model
+
+class MedidaController:
+    def __init__(self):
+        self.model = medida_model.MedidaModel()
+
+    def inserir_medida(self, unidade):
+        sql = f"INSERT INTO medida(med_unidade) VALUES ('{unidade}');"
+        return self.model.insert(sql)
+
+    def listar_medida(self, unidade=''):
+        sql = f'SELECT * FROM medida WHERE med_unidade LIKE "%{unidade}%";'
+        return self.model.get(sql)
+
+    def excluir_medida(self, id):
+        sql = f'DELETE FROM medida WHERE med_id = {id}'
+        return self.model.delete(sql)
+
+    def atualizar_medida(self, id, unidade):
+        sql = f"UPDATE medida SET med_unidade = '{unidade}' WHERE med_id = {id};"
+        return self.model.update(sql)
