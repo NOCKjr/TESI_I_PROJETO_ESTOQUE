52ba8397db15ce35752e64b7b0a8906f2eccac1f
AlonsoMCDG <alonsomartins056@gmail.com>
2025-09-30 12:29:56 -0500

    doc: adicionando comentários descrevendo os métodos do model

--- model/conexao.py
+++ model/conexao.py
@@ -3,6 +3,8 @@ from sqlite3 import Error
 
 class Conexao:
     def get_conexao(self):
+        """Retorna um objeto de conexão com o banco de dados"""
+        
         caminho = 'banco/banco.db'
         try:
             con = sqlite3.connect(caminho)
