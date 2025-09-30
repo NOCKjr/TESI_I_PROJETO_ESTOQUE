cc1d035dc314ebe83abb94b7ffa12f71d6d68bb3
nock_jr <eliascruznocjr123@gmail.com>
2025-09-29 19:38:47 -0500

    feat: listar movimentacao por fornecedor

--- control/movimentacao_controller.py
+++ control/movimentacao_controller.py
@@ -8,8 +8,12 @@ class MovimentacaoController:
         sql = f"INSERT INTO movimentacao(mov_data, mov_tipo,fk_mov_usu_id, fk_mov_for_id, fk_mov_esc_id) VALUES ('{data}', '{tipo}', {usuario_id}, {fornecedor_id}, {escola_id});"
         return self.model.insert(sql)
 
-    def listar_movimentacao(self, unidade=''):
+    def listar_movimentacao(self):
         sql = f"SELECT * FROM movimentacao ORDER BY mov_data DESC"
+        return self.model.get(sql)
+
+    def listar_movimentacao_por_fornecedor(self, fornecedor_id):
+        sql = f"SELECT * FROM movimentacao WHERE fk_mov_for_id = {fornecedor_id} ORDER BY mov_data DESC"
         return self.model.get(sql)
 
     def excluir_movimentacao(self, id):
@@ -17,3 +21,6 @@ class MovimentacaoController:
 
     def atualizar_movimentacao(self, id, unidade):
         pass
+
+#teste = MovimentacaoController()
+#print(teste.listar_movimentacao_por_fornecedor(3))
